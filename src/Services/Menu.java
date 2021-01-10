14dc0b925c3068cbf7a60a01b8aa910b69e22061
unknown <quentinprs62@gmail.com>
2021-01-10 15:18:50 +0100

    add Menu and modification class.java

---
+++ src/Services/Menu.java
@@ -0,0 +1,42 @@
+package Services;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    public static void menu(){
+        int choice;
+        do {
+            System.out.println("--------Menu-------- \n"+
+                    "1: Play \n"+
+                    "2: Settings\n"+
+                    "3. Exit \n"+
+                    "Entre ton choix! "
+            );
+
+            Scanner scanner = new Scanner(System.in);
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    // Play
+                    System.out.println("coucou je suis la");
+                    choice=3;
+                    break;
+                case 2:
+                    // Change settings
+                    System.out.println("coucou je suis la dans le binks");
+                    choice=3;
+                    break;
+                case 3:
+                    // Exit games
+                    System.out.println("You quit the game i hope to see you soon");
+                    break;
+                default:
+                    // The user input an unexpected choice.
+                    System.out.println(("Wrong Choice. Enter again\n"));
+                    break;
+            }
+        }while (choice!=3);
+    }
+}
